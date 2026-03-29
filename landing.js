f7ef9a4b451e53dcee09132d992f6a7da51f63d8
mshinde-iterforge <mayur.shinde@iterforge.com>
2026-03-29 09:40:56 +0530

    Remove category click event listener
    Removed event listener for category clicks.

--- landing.js
+++ landing.js
@@ -90,14 +90,3 @@ loadCategories();
 loadProducts();
 
 
-categoryContainer.addEventListener("click", (e)=>{
-    console.log("Listening");
-
-    document.querySelectorAll(".category-pill").forEach(pill => pill.classList.remove("active"));
-
-    e.target.classList.add("active");
-    const categoryId = e.target.dataset.id;
-    console.log(categoryId);
-    loadProducts(categoryId);
-    alert("Category Clicked");
-});
